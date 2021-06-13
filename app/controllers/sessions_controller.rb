6b04bbfc71d17850d9bd6b23408619aaf904ca1e
norah ngatjizeko <tjeripo86@live.com>
2021-06-13 16:00:07 +0900

    bootstrap and jquery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
+  before_action :login_required, :except=>[:new, :create]
   def new
   end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
     else
       flash[:danger] = 'I failed to login'
       render :new
