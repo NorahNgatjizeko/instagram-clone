d070d1ce9b290d0bf218c220ec635462c9f086f0
norah ngatjizeko <tjeripo86@live.com>
2021-06-15 16:28:52 +0900

    feed update

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,58 +1,58 @@
 class FeedsController < ApplicationController
-  before_action :set_feed, only: %i[ show edit update destroy ]
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
+  before_action :user_login_check, only: [:new]
 
-  # GET /feeds or /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1 or /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
-    @feed = Feed.new
+    if params[:back]
+      @feed = Feed.new(feed_params)
+    else
+      @feed = Feed.new
+    end
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds or /feeds.json
   def create
-    @feed = Feed.new(feed_params)
-
-    respond_to do |format|
+    @feed = current_user.feeds.build(feed_params)
+    if params[:back]
+      render :new
+    else
       if @feed.save
-        format.html { redirect_to @feed, notice: "Feed was successfully created." }
-        format.json { render :show, status: :created, location: @feed }
+        redirect_to @feed, notice: 'Feed was posted'
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
 
-  # PATCH/PUT /feeds/1 or /feeds/1.json
+  def confirm
+    @feed = current_user.feeds.build(feed_params)
+    render :new if @feed.invalid?
+  end
+  # PATCH/PUT /feeds/1
+  # PATCH/PUT /feeds/1.json
   def update
     respond_to do |format|
       if @feed.update(feed_params)
-        format.html { redirect_to @feed, notice: "Feed was successfully updated." }
-        format.json { render :show, status: :ok, location: @feed }
+        format.html { redirect_to @feed, notice: 'Feed was updated' }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
 
-  # DELETE /feeds/1 or /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
+      format.html {redirect_to feeds_url, notice: 'Feed was deleted'}
     end
   end
 
@@ -64,6 +64,12 @@ class FeedsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def feed_params
-      params.require(:feed).permit(:image)
+      params.require(:feed).permit(:content, :image, :image_cache)
     end
+
+    def user_login_check
+   unless logged_in?
+     redirect_to root_path
+   end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :feeds
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources  :users
+
+  resources :feeds do
+    collection do
+    post :confirm
+  end
+end
 end
